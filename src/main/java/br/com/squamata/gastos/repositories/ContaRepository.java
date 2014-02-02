6e9020dffc929bad40372a47e30c01b190d8d8ce
FelipeAdorno <felipeadsc@gmail.com>
2014-02-01 23:29:42 -0200

    Novas alterações

--- src/main/java/br/com/squamata/gastos/repositories/ContaRepository.java
+++ src/main/java/br/com/squamata/gastos/repositories/ContaRepository.java
@@ -1,5 +1,8 @@
 package br.com.squamata.gastos.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +13,13 @@ import br.com.squamata.gastos.domain.Conta;
 
 @Repository
 public interface ContaRepository extends PagingAndSortingRepository<Conta,ObjectId> {
+	
+	List<Conta> findByUsuarioNomeUsuario(String nomeUsuario);
 
 	Page<Conta> findByUsuarioNomeUsuario(String nomeUsuario, Pageable pageable);
 	
 	Conta findByUsuarioNomeUsuarioAndDescricao(String nomeUsuario, String categoria);
+	
+	Page<Conta> findByUsuarioNomeUsuarioAndVencimentoBetween(String nomeUsuario, Date dataInicial, Date dataFinal, Pageable pageable);
 	
 }
