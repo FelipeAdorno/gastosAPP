620c7f05df0542c49e588192e97394b6f8d7d1cc
FelipeAdorno <felipeadsc@gmail.com>
2014-02-01 23:30:05 -0200

    Novas alterações

--- src/main/java/br/com/squamata/gastos/service/ContaService.java
+++ src/main/java/br/com/squamata/gastos/service/ContaService.java
@@ -1,8 +1,11 @@
 package br.com.squamata.gastos.service;
 
-import br.com.squamata.gastos.domain.Conta;
+import br.com.squamata.gastos.exception.UsuarioSessaoNullException;
 import br.com.squamata.gastos.vo.ContaListaVO;
+import br.com.squamata.gastos.vo.ContaVO;
 
-public interface ContaService extends AbstractService<Conta, ContaListaVO> {
+public interface ContaService extends AbstractService<ContaVO, ContaListaVO> {
+
+	ContaListaVO buscarPorMesEAno(final Integer paginaAtual, final Integer quantidadeRegistros, final Integer mes, final Integer ano) throws UsuarioSessaoNullException; 
 	
 }
