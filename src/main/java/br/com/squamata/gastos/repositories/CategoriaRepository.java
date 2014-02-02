59d29fdd876aa9c624f50f6c9fcc734b34a28085
FelipeAdorno <felipeadsc@gmail.com>
2014-02-01 23:29:36 -0200

    Novas alterações

--- src/main/java/br/com/squamata/gastos/repositories/CategoriaRepository.java
+++ src/main/java/br/com/squamata/gastos/repositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package br.com.squamata.gastos.repositories;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,8 @@ import br.com.squamata.gastos.domain.Categoria;
 
 @Repository
 public interface CategoriaRepository extends PagingAndSortingRepository<Categoria,ObjectId> {
+	
+	List<Categoria> findByUsuarioNomeUsuario(String nomeUsuario);
 
 	Page<Categoria> findByUsuarioNomeUsuario(String nomeUsuario, Pageable pageable);
 	
