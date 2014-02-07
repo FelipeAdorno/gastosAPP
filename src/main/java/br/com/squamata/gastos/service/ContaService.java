b8b575448a06c013e4e3277de71b159504cafac3
FelipeAdorno <felipeadsc@gmail.com>
2014-02-07 01:36:58 -0200

    Novas funcionalidades

--- src/main/java/br/com/squamata/gastos/service/ContaService.java
+++ src/main/java/br/com/squamata/gastos/service/ContaService.java
@@ -1,11 +1,24 @@
 package br.com.squamata.gastos.service;
 
+import java.util.List;
+
 import br.com.squamata.gastos.exception.UsuarioSessaoNullException;
 import br.com.squamata.gastos.vo.ContaListaVO;
 import br.com.squamata.gastos.vo.ContaVO;
+import br.com.squamata.gastos.vo.TotalContaVO;
 
 public interface ContaService extends AbstractService<ContaVO, ContaListaVO> {
 
-	ContaListaVO buscarPorMesEAno(final Integer paginaAtual, final Integer quantidadeRegistros, final Integer mes, final Integer ano) throws UsuarioSessaoNullException; 
+	ContaListaVO buscarPorMesEAno(Integer paginaAtual, Integer quantidadeRegistros, Integer mes, Integer ano) throws UsuarioSessaoNullException; 
+	
+	ContaListaVO buscarContasDaSemana(Integer paginaAtual, Integer quantidadeRegistros) throws UsuarioSessaoNullException; 
+	
+	TotalContaVO buscarTotalMesAtual() throws UsuarioSessaoNullException;
+	
+	TotalContaVO buscarTotalMes(Integer mes, Integer ano) throws UsuarioSessaoNullException;
+	
+	List<ContaVO> listarContasMes(Integer mes, Integer ano) throws UsuarioSessaoNullException;
+	
+	TotalContaVO buscarTotalDividas() throws UsuarioSessaoNullException;
 	
 }
