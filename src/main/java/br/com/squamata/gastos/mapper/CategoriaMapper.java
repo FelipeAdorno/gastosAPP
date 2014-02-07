db5e309fa32c4017bc101bf2621425f6b843e6be
FelipeAdorno <felipeadsc@gmail.com>
2014-02-07 01:35:45 -0200

    Novas funcionalidades

--- src/main/java/br/com/squamata/gastos/mapper/CategoriaMapper.java
+++ src/main/java/br/com/squamata/gastos/mapper/CategoriaMapper.java
@@ -2,7 +2,26 @@ package br.com.squamata.gastos.mapper;
 
 import org.springframework.stereotype.Component;
 
+import br.com.squamata.gastos.domain.Categoria;
+import br.com.squamata.gastos.vo.CategoriaVO;
+
 @Component
 public class CategoriaMapper {
 
+
+	public Categoria mapearCategoriaVOEmCategoria(final CategoriaVO entrada) {
+		
+		Categoria categoria = new Categoria();
+		categoria.setCategoria(entrada.getCategoria());
+		categoria.setId(entrada.getId());
+		return categoria;
+	}
+	
+	public CategoriaVO mapearCategoriaEmCCategoriaVO(final Categoria entrada) {
+		CategoriaVO categoriaVO = new CategoriaVO();
+		categoriaVO.setCategoria(entrada.getCategoria());
+		categoriaVO.setId(entrada.getId());
+		return categoriaVO;
+	}
+	
 }
