b6483cd8264b060ef67b908f8eb59ae44ef5a1d3
FelipeAdorno <felipeadsc@gmail.com>
2014-02-07 01:38:10 -0200

    Novas funcionalidades

--- src/main/webapp/resources/js/Controller/GastosController.js
+++ src/main/webapp/resources/js/Controller/GastosController.js
@@ -1,13 +1,53 @@
 function GastosController($scope, $location, $http) {
 	
-	$scope.urlPrefix = "/squamataGastos/";
+	$scope.urlPrefix = "/squamataGastos/gastos";
 	
 	$scope.usuario = {};
+	
+	$scope.contasSemana = {};
+	
+	$scope.totalMes = {};
+	
+	$scope.totalGeral = {};
 	
 	$scope.iniciar = function () {
 	    Main.ativarMenu("menuHome");
 	};
+	
+	
+	$scope.buscarContasSemana = function() {
+		var url = $scope.urlPrefix + "/buscarContasSemana";
+        $http.get(url)
+        .success(function (retorno) {
+        	$scope.contasSemana = retorno.contas;
+        })
+        .error(function() {
+        	MensagemRetorno.tratarMensagemErroComunicacao();
+        });
+	};
+	
+	$scope.buscarTotalMes = function() {
+		var url = $scope.urlPrefix + "/totalMes";
+        $http.get(url)
+        .success(function (retorno) {
+        	$scope.totalMes = retorno;
+        })
+        .error(function() {
+        	MensagemRetorno.tratarMensagemErroComunicacao();
+        });
+	};
     
+	$scope.buscarTotalGeral = function() {
+		var url = $scope.urlPrefix + "/totalDividas";
+        $http.get(url)
+        .success(function (retorno) {
+        	$scope.totalGeral = retorno;
+        })
+        .error(function() {
+        	MensagemRetorno.tratarMensagemErroComunicacao();
+        });
+	};
+	
     $scope.iniciar();
     
 };
